1b2b17f95f3f8d192685b5c200d6cceaec0a4ebb
Malcolm MacLure <39014058+MacLure@users.noreply.github.com>
2018-07-08 13:54:03 -0400

    Add files via upload

---
+++ basic-maths.rb
@@ -0,0 +1,34 @@
+print "Adding:  Enter a value: "
+first_num = gets.to_i
+
+print "Enter another value: "
+second_num = gets.to_i
+puts first_num.to_s + " + " + second_num.to_s + " = " + (first_num + second_num).to_s
+
+print "Subtracting:  Enter a value: "
+first_num = gets.to_i
+
+print "Enter another value: "
+second_num = gets.to_i
+puts first_num.to_s + " - " + second_num.to_s + " = " + (first_num - second_num).to_s
+
+print "Multiplying:  Enter a value: "
+first_num = gets.to_i
+
+print "Enter another value: "
+second_num = gets.to_i
+puts first_num.to_s + " x " + second_num.to_s + " = " + (first_num * second_num).to_s
+
+print "Dividing:  Enter a value: "
+first_num = gets.to_i
+
+print "Enter another value: "
+second_num = gets.to_i
+puts first_num.to_s + " ÷ " + second_num.to_s + " = " + (first_num / second_num).to_s
+
+print "Exponent:  Enter a value: "
+first_num = gets.to_i
+
+print "Enter another value: "
+second_num = gets.to_i
+puts first_num.to_s + " to the power of " + second_num.to_s + " = " + (first_num ** second_num).to_s
